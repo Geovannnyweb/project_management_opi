0505b57c99424f4d14e2e78ec09a2c19ea6c1438
Geovanny Camargo <giovani.camargor@opitech.com.co>
2023-11-03 07:42:21 -0500

    feat(SBModelCreation): Add Model Creation

---
+++ Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectManagement.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid ProjectId { get; set; }
+
+        [ForeignKey("ProjectCategoryId")]
+        public Guid ProjectCategoryId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public virtual ProjectCategory Category { get; set; }
+
+    }
+
+    public enum Status
+    {
+        ToDo,
+        InProgress,
+        Qa,
+        Done
+    }
+}
---
+++ Models/ProjectCategory.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace ProjectManagement.Models
+{
+    public class ProjectCategory
+    {
+        public Guid ProjectCategoryId { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Project> Project { get; set; }
+
+    }
+}
